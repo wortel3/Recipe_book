7dee11e09480b321afff071be8e80db228e37baa
Gerhard Van Deventer <vandeventerg@out.co.za>
2017-02-07 14:29:23 +0200

    Working on it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent} from './header.component';
 
 @Component({
   selector: 'rb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rb works!';
+export class AppComponent { 
 }
